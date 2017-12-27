15e06103c3cbfe24fbe0aa3b972dde91cd7f8104
Farid Ansari <farid12ansari7@gmail.com>
2017-12-27 23:36:04 +0530

    js structure init

---
+++ app.js
@@ -0,0 +1,13 @@
+//Budget Controller
+let budgetController = (function() {
+  //....
+})();
+
+//UI Controller
+let UIController = (function() {
+  //...
+})();
+
+let controller = (function(budgetCtrl, UICtrl) {
+  //...
+})(budgetController, UIController);
