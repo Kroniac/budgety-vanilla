401546c29181adf4cfad5832de8102a223580684
Farid Ansari <farid12ansari7@gmail.com>
2017-12-29 05:35:23 +0530

    clear fields after adding the item

--- app.js
+++ app.js
@@ -99,6 +99,16 @@ let UIController = (() => {
       // Insert the HTML into the DOM
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
+    clearFields: () => {
+      let fields, fieldsArr;
+      fields = document.querySelectorAll(
+        DOMstrings.inputDescription + ', ' + DOMstrings.inputValue
+      );
+      fieldsArr = Array.prototype.slice.call(fields);
+      fieldsArr.forEach((current, index, array) => {
+        current.value = '';
+      });
+    },
     getDOMstrings: () => {
       return DOMstrings;
     }
@@ -130,8 +140,11 @@ let controller = ((budgetCtrl, UICtrl) => {
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     //3. Add the item to the UI
     UICtrl.addListItem(newItem, input.type);
-    //4. Calculate the budget
-    //5. Display the budget on the UI
+
+    //4.Clear the fields
+    UICtrl.clearFields();
+    //5. Calculate the budget
+    //6. Display the budget on the UI
   };
 
   return {
